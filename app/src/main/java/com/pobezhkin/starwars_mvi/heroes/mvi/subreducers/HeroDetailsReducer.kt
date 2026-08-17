ca401ba10f19cc87bc19ce0a34188c3d386ca132
Roman Pobezhkin <81640897+RomanBoris@users.noreply.github.com>
2026-08-17 09:12:51 +0300

    HeroDetailsReducer решает проблему - гонка между сетью и кнопкой «Назад»

---
+++ app/src/main/java/com/pobezhkin/starwars_mvi/heroes/mvi/subreducers/HeroDetailsReducer.kt
@@ -0,0 +1,33 @@
+package com.pobezhkin.starwars_mvi.heroes.mvi.subreducers
+
+import com.pobezhkin.starwars_mvi.core.mvi.Reducer
+import com.pobezhkin.starwars_mvi.heroes.mvi.HeroesEffect
+import com.pobezhkin.starwars_mvi.heroes.mvi.HeroesState
+import com.pobezhkin.starwars_mvi.heroes.mvi.HeroesState.Step
+
+internal class HeroDetailsReducer: Reducer<HeroesEffect.Details, HeroesState> {
+    override fun invoke(
+        effect: HeroesEffect.Details,
+        state: HeroesState): HeroesState =
+        when(effect){
+            is HeroesEffect.Details.Loaded -> when(state.step){
+                is Step.DetailsLoading -> state.copy(
+                    step = Step.Details(effect.hero, effect.fromCache)
+                )
+                else -> state
+            }
+
+            is HeroesEffect.Details.LoadFailed -> when(val current = state.step){
+                is Step.DetailsLoading -> state.copy(
+                    step = Step.DetailsError(heroUrl = current.heroUrl, error = effect.error)
+                )
+                else -> state
+            }
+
+            is HeroesEffect.Details.Back -> state.copy(
+                step = state.previousListStep ?: Step.ListLoading,
+                previousListStep = null
+            )
+            else -> state
+    }
+}
